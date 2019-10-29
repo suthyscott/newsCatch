67947dd607b2292551035b4897324b2067fb5622
unknown <sutherlandscott21@gmail.com>
2019-10-29 17:43:47 -0600

    switched dropdown menu to ternary toggle

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,55 +4,47 @@ import './Header.css';
 import {Link} from 'react-router-dom';
 
 function Header(){
-
-    let dropdown = React.createRef()
-    // console.log(dropdown)
+    const [showMenu, setShowMenu] = useState(false)
 
     const toggleMenu = () => {
-        const {current} = dropdown
-        console.log(current.classList)
-
-
-        if(current.classList.contains('show-animation')){
-            current.classList.add('hide-animation')
-            current.classList.remove('show-animation')
-        } else {
-            current.classList.add('show-animation')
-            current.classList.remove('hide-animation')
-        }
+        setShowMenu(!showMenu)
     }
 
     return(
         <main  className='header'>
+            
             <header>
                 <h1>NewsCatch</h1>
             </header>
+            <div id='menu-hamburger-icon'>
+                <i id='hamburger-icon' className='fas fa-bars fa-2x' onClick={toggleMenu} />
+            </div>
+
+            {showMenu ? (
+                <nav className='dropdown-menu'>
+
+                    <section id='nav-buttons-dropdown'>
+                        <p id='user-email'>user_email@email.com</p>
+                            <Link to='/myaccount'><button className='link'>My Account</button></Link>
+                            <Link to='/home'><button className='link'>Home</button></Link>
+                            <Link to='/savedarticles'><button className='link'>Saved Articles</button></Link>
+                            <Link to='/'><button className='link'>Sign Out</button></Link>
+                    </section>
+                </nav>
+            ) : null}
 
             <nav >
-                <div id='nav-bar'>
-                
-                    <section className='nav-buttons'>
-                        user_email@email.com
-                        <button>My Account</button>
-                        <button>Home</button>
-                        <button>Saved Articles</button>
-                        <button>Sign Out</button>
+
+                    <section id='nav-buttons'>
+                        <Link to='/myaccount'><button className='nav-link'>My Account</button></Link>
+                        <Link to='/home'><button className='nav-link'>Home</button></Link>
+                        <Link to='/savedarticles'><button className='nav-link'>Saved Articles</button></Link>
+                        <Link to='/'><button className='nav-link'>Sign Out</button></Link>
                     </section>
-                </div>
-                    <div id='menu-hamburger-icon'><i id='hamburger-icon' className='fas fa-bars fa-2x' onClick={toggleMenu} />
-                </div>
-
+                    <section id='user-email-display'>
+                        <p id='user-email'>user_email@email.com</p>
+                    </section>
             </nav>
-            <div className='dropdown-menu show-animation' ref={dropdown}>
-
-                <section id='nav-buttons-dropdown'>
-                    <p id='user-email'>useremail@email.com</p>
-                    <Link to='/myaccount'><button className='link'>My Account</button></Link>
-                    <Link to='/home'><button className='link'>Home</button></Link>
-                    <Link to='/savedarticles'><button className='link'>Saved Articles</button></Link>
-                    <Link to='/'><button className='link'>Sign Out</button></Link>
-                </section>
-            </div>
 
         </main>
     )

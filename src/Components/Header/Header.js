33018067c44e6d94cd01dfafb6e9341b816e30b7
unknown <sutherlandscott21@gmail.com>
2019-10-29 21:03:02 -0600

    Added header to landing and register pages

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import '../../reset.css';
 import './Header.css';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
-function Header(){
+function Header(props){
     const [showMenu, setShowMenu] = useState(false)
+    const [login, setLogin] = useState(false)
+
 
     const toggleMenu = () => {
         setShowMenu(!showMenu)
     }
 
+    const toggleLogin = () => {
+        setLogin(!login)
+    }
+
+    if(props.location.pathname !== '/' && props.location.pathname !=='/register'){
     return(
         <main  className='header'>
             
@@ -25,15 +32,15 @@ function Header(){
 
                     <section id='nav-buttons-dropdown'>
                         <p id='user-email'>user_email@email.com</p>
-                            <Link to='/myaccount'><button className='link'>My Account</button></Link>
-                            <Link to='/home'><button className='link'>Home</button></Link>
-                            <Link to='/savedarticles'><button className='link'>Saved Articles</button></Link>
-                            <Link to='/'><button className='link'>Sign Out</button></Link>
+                            <Link to='/myaccount'><button className='link' onClick={() => toggleMenu()}>My Account</button></Link>
+                            <Link to='/home'><button className='link' onClick={() => toggleMenu()}>Home</button></Link>
+                            <Link to='/savedarticles'><button className='link' onClick={() => toggleMenu()}>Saved Articles</button></Link>
+                            <Link to='/'><button className='link' onClick={() => toggleMenu()}>Sign Out</button></Link>
                     </section>
                 </nav>
             ) : null}
 
-            <nav >
+            <nav id='nav-menu'>
 
                     <section id='nav-buttons'>
                         <Link to='/myaccount'><button className='nav-link'>My Account</button></Link>
@@ -46,8 +53,32 @@ function Header(){
                     </section>
             </nav>
 
+
+        </main>
+           
+    )
+} else {
+    return(
+        <main className='header'>
+                <h1>NewsCatch</h1>
+
+                {!login ? (
+                    <nav id='nav-buttons-landing'>
+                        <button className='nav-link' onClick={() => toggleLogin()}>Login</button>
+                        <Link to='/register' ><button className='nav-link'>Register</button></Link>
+                    </nav>
+                ) : (
+                    <div>
+                        <input 
+                        placeholder='email' />
+                        <input
+                        placeholder='password' />
+                        <button id='login-button'>Login</button>
+                    </div>
+                )}
         </main>
     )
 }
+}
 
-export default Header
+export default withRouter(Header)
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Landing(){
     return(
         <div>
             Landing
+            <Link to='/home'>Login</Link>
         </div>
     )
 }
